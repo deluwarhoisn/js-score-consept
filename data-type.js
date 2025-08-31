fb7a60875dcc7dedb973c2dcf264188d15459f11
deluwarhoisn <babuhossen301@gmail.com>
2025-08-31 20:31:20 +0600

    data-types

---
+++ data-type.js
@@ -0,0 +1,21 @@
+let data = 5;
+data = 'tormuj';
+data = false;
+data = {name:'banngi',price:40}
+data = [445]
+data = undefined
+data = null
+console.log(typeof data)
+
+//  js: primitive data types
+const price = 50;
+const name = 'bodrul';
+const isPoor = false;
+let isKnown = undefined;  //usually you not set value to undefined
+let isLocal;
+console.log(isKnown,isLocal)
+//  non-primitive data type
+
+
+const student = {name:'babul', location:'bristy goli',subject:'botany'}
+const friends =['tom','bob','jack','rock']
