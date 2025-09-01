b476897abeefe70c1b10484f7bf791674b79025b
deluwarhoisn <babuhossen301@gmail.com>
2025-09-01 22:03:41 +0600

    refactor increment.js to uncomment counter increment examples for clarity

--- increment.js
+++ increment.js
@@ -1,10 +1,10 @@
-// let counter = 0;
-// console.log(counter = counter + 1)
-// console.log(counter += 1)
-// console.log(counter ++)
-// console.log(++ counter)
-// console.log(counter)
-
-
 let counter = 0;
 console.log(counter = counter + 1)
+console.log(counter += 1)
+console.log(counter ++)
+console.log(++ counter)
+console.log(counter)
+
+
+let counterr = 0;
+console.log(counterr = counterr + 1)
